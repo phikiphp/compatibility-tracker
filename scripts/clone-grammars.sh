a923e2c2d351f2434f8d62bb55616ba20e412a80
Ryan Chandler <ryangjchandler@gmail.com>
2025-08-05 22:10:19 +0100

    Update script

--- scripts/clone-grammars.sh
+++ scripts/clone-grammars.sh
@@ -3,5 +3,6 @@
 echo "Cloning grammars repository..."
 
 git clone --depth=1 --single-branch --branch=main git@github.com:shikijs/textmate-grammars-themes.git
+rm -rf textmate-grammars-themes/.git
 
 echo "Cloned grammars into textmate-grammars-themes directory."
