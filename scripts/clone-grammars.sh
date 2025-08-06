36d4e3fe4225eaf8e160ce23da4b5c97a7579cdc
Ryan Chandler <ryangjchandler@gmail.com>
2025-08-06 13:26:57 +0100

    Update clone grammars script

--- scripts/clone-grammars.sh
+++ scripts/clone-grammars.sh
@@ -2,7 +2,13 @@
 
 echo "Cloning grammars repository..."
 
-git clone --depth=1 --single-branch --branch=main git@github.com:shikijs/textmate-grammars-themes.git
+if [[ -n $GH_TOKEN ]]; then
+  GIT_REPO_URL="https://x-access-token:${GH_TOKEN}@github.com/shikijs/textmate-grammars-themes.git"
+else
+  GIT_REPO_URL="git@github.com:shikijs/textmate-grammars-themes.git"
+fi
+
+git clone --depth=1 --single-branch --branch=main "$GIT_REPO_URL" textmate-grammars-themes
 rm -rf textmate-grammars-themes/.git
 
 echo "Cloned grammars into textmate-grammars-themes directory."
